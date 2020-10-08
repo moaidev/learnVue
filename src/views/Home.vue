6ebd948d68410ed1b3a214de823c333b7264a43c
moaidev <seonjunwoo@naver.com>
2020-10-08 16:11:48 +0900

    test

---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h1>Welcome to {{title}}!</h1>
+        <input type="text" v-model="input1" />
+        <button type="button" @click="getData">Get</button>
+        <button type="button" @click="setData">Set</button>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            title: "누누누누",
+            title2: "Incheon",
+            input1: "abc",
+        };
+    },
+    methods: {
+        getData() {
+            alert(this.input1)
+        },
+        setData() {
+            this.input1 = "12345"
+        }
+    },
+    beforeCreate() {
+        console.log("beforeCreate")
+    },
+    created() {
+        console.log("created")
+    },
+    beforeMount() {
+        console.log("beforeMount")
+    },
+    mounted() {
+        console.log("mounted")
+    },
+    beforeUpdate() {
+        console.log("beforeUpdate")
+    },
+    updated() {
+        console.log("updated")
+    },
+    beforeDestroy() {
+        console.log("beforeDestroy")
+    },
+    destroyed() {
+        console.log("destroyed")
+    }
+}
+</script>
